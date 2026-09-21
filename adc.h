f7f4699da4f3937f86dc8ca2321cb2341d69dae4
souhaieb elamri <souhaiebelamri78@gmail.com>
2026-09-21 18:55:27 +0100

    Add files via upload

---
+++ adc.h
@@ -0,0 +1,15 @@
+#ifndef ADC_H
+#define ADC_H
+
+#include "stm32f4xx.h"
+
+extern uint16_t adc_val[3];
+extern volatile uint8_t adc_ready;
+
+void Config_TIM2(void);
+void Config_DMA2_ADC1(void);
+void config_adc1(void);
+void TIM2_IRQHandler(void);
+void DMA2_Stream0_IRQHandler(void);
+
+#endif
